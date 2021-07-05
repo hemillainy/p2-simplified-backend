af2017679f61f1f9ca5128037cf1d3659cc45836
Hemillainy Santos <hemillainy.santos@ccc.ufcg.edu.br>
2021-07-05 10:07:38 -0300

    update tests

--- repository/database/database_test.go
+++ repository/database/database_test.go
@@ -3,54 +3,67 @@ package database
 import (
 	"context"
 	"github.com/google/uuid"
-	"github.com/hemillainy/backend/config"
 	repo "github.com/hemillainy/backend/repository"
 	"github.com/hemillainy/backend/schemas"
+	"os"
 	"testing"
 )
 
-func insertDefault(r repo.IRepository, u schemas.User, s schemas.Shopkeeper) func() {
-	r.CreateUser(context.Background(), u)
-	r.CreateShopkeeper(context.Background(), s)
+func setEnvs() {
+	os.Setenv("BACKEND_DB_HOST", "localhost")
+	os.Setenv("BACKEND_DB_SCHEME", "postgres")
+	os.Setenv("BACKEND_USER", "postgres")
+	os.Setenv("BACKEND_PASSWORD", "postgres")
+	os.Setenv("BACKEND_DB_PORT", "5432")
+	os.Setenv("BACKEND_DB_SCHEME", "postgres")
+	os.Setenv("BACKEND_DB_NAME", "backend")
+}
+
+func insertDefault(r repo.IRepository, user schemas.User, shopkeeper schemas.User) func() {
+	r.CreateUser(context.Background(), user)
+	r.CreateUser(context.Background(), shopkeeper)
 	return func() {
-		r.DeleteUser(context.Background(), u.UUID)
-		r.DeleteShopkeeper(context.Background(), s.UUID)
+		r.DeleteUser(context.Background(), user.UUID)
+		r.DeleteUser(context.Background(), shopkeeper.UUID)
 	}
 }
 
 func TestCreateTransfer(t *testing.T) {
-	repo, err := Open(context.Background(), &config.Config{})
+	setEnvs()
+	repo, err := Open(context.Background())
 	if err != nil {
 		t.Errorf("no error expected but has: %v", err)
 		return
 	}
 
-	user := schemas.User{
-		UUID:     uuid.NewString(),
-		Name:     "User Test",
-		CPF:      "11111111111",
-		Email:    "user@backend.com",
-		Password: "senha123",
-		Wallet:   200,
+	commonUser := schemas.User{
+		UUID:       uuid.NewString(),
+		Name:       "User Test",
+		Document:   "11111111111",
+		Email:      "user@backend.com",
+		Password:   "senha123",
+		Wallet:     200,
+		CommonUser: true,
 	}
-	shopkeeper := schemas.Shopkeeper{
-		UUID:     uuid.NewString(),
-		Name:     "Shopkeeper",
-		CNPJ:     "11111111111111",
-		Email:    "shopkeeper@backend.com",
-		Password: "senha123",
-		Wallet:   0,
+	shopkeeper := schemas.User{
+		UUID:       uuid.NewString(),
+		Name:       "Shopkeeper Test",
+		Document:   "11111111111111",
+		Email:      "shopkeeper@backend.com",
+		Password:   "senha123",
+		Wallet:     0,
+		CommonUser: false,
 	}
 
-	_ = insertDefault(repo, user, shopkeeper)
-	//defer f()
+	f := insertDefault(repo, commonUser, shopkeeper)
+	defer f()
 
 	value := 100.00
 	status := "done"
 
-	_, err = repo.CreateTransfer(context.Background(), schemas.Transfer{
+	transferCreated, err := repo.CreateTransfer(context.Background(), schemas.Transfer{
 		Value:  value,
-		Payer:  user.UUID,
+		Payer:  commonUser.UUID,
 		Payee:  shopkeeper.UUID,
 		Status: status,
 	})
@@ -59,9 +72,80 @@ func TestCreateTransfer(t *testing.T) {
 		t.Errorf("error no expected but has %v", err.Error())
 		return
 	}
+
+	if value != transferCreated.Value {
+		t.Errorf("expected %v but has %v", value, transferCreated.Value)
+		return
+	}
 }
 
 func TestRepository_UpdateUserWallet(t *testing.T) {
+	setEnvs()
+	repo, err := Open(context.Background())
+	if err != nil {
+		t.Errorf("no error expected but has: %v", err)
+		return
+	}
 
+	defer repo.Close()
+
+	commonUser := schemas.User{
+		UUID:       uuid.NewString(),
+		Name:       "User Test",
+		Document:   "11111111111",
+		Email:      "user@backend.com",
+		Password:   "senha123",
+		Wallet:     200,
+		CommonUser: true,
+	}
+	shopkeeper := schemas.User{
+		UUID:       uuid.NewString(),
+		Name:       "Shopkeeper Test",
+		Document:   "11111111111111",
+		Email:      "shopkeeper@backend.com",
+		Password:   "senha123",
+		Wallet:     0,
+		CommonUser: false,
+	}
+
+	f := insertDefault(repo, commonUser, shopkeeper)
+	defer f()
+
+	testCases := []struct {
+		description string
+		value       float64
+		expected    float64
+		commonUser  bool
+		user        schemas.User
+	}{
+		{
+			description: "decreases user wallet",
+			value:       100,
+			expected:    100,
+			commonUser:  true,
+			user:        commonUser,
+		},
+		{
+			description: "increase the user's wallet",
+			value:       100,
+			expected:    100,
+			commonUser:  false,
+			user:        shopkeeper,
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.description, func(t *testing.T) {
+			u, err := repo.UpdateUserWallet(context.Background(), test.value, test.user.UUID, test.commonUser)
+			if err != nil {
+				t.Errorf("no error expected but has: %v\n", err)
+				return
+			}
+
+			if test.expected != u.Wallet {
+				t.Errorf("expected %v but has %v", test.expected, u.Wallet)
+				return
+			}
+		})
+	}
 }
-
--- transfer/transfer_test.go
+++ transfer/transfer_test.go
@@ -5,7 +5,6 @@ import (
 	"context"
 	"encoding/json"
 	"github.com/google/uuid"
-	"github.com/hemillainy/backend/config"
 	error2 "github.com/hemillainy/backend/error"
 	repo "github.com/hemillainy/backend/repository"
 	"github.com/hemillainy/backend/repository/database"
@@ -13,53 +12,67 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 )
 
-func insertDefault(r repo.IRepository, u schemas.User, s schemas.Shopkeeper) func() {
-	r.CreateUser(context.Background(), u)
-	r.CreateShopkeeper(context.Background(), s)
+func setEnvs() {
+	os.Setenv("BACKEND_DB_HOST", "localhost")
+	os.Setenv("BACKEND_DB_SCHEME", "postgres")
+	os.Setenv("BACKEND_USER", "postgres")
+	os.Setenv("BACKEND_PASSWORD", "postgres")
+	os.Setenv("BACKEND_DB_PORT", "5432")
+	os.Setenv("BACKEND_DB_SCHEME", "postgres")
+	os.Setenv("BACKEND_DB_NAME", "backend")
+}
+
+func insertDefault(r repo.IRepository, commonUser schemas.User, shopkeeper schemas.User) func() {
+	r.CreateUser(context.Background(), commonUser)
+	r.CreateUser(context.Background(), shopkeeper)
 	return func() {
-		r.DeleteUser(context.Background(), u.UUID)
-		r.DeleteShopkeeper(context.Background(), s.UUID)
+		r.DeleteUser(context.Background(), commonUser.UUID)
+		r.DeleteUser(context.Background(), shopkeeper.UUID)
 	}
 }
 
-func TestMakeTransfer(t *testing.T) {
-	r, err := database.Open(context.Background(), &config.Config{})
+func TestTransactionHandler(t *testing.T) {
+	setEnvs()
+	r, err := database.Open(context.Background())
 	if err != nil {
 		t.Errorf("expected no errors but was %v", err)
 		return
 	}
 
-	user := schemas.User{
-		UUID:     uuid.NewString(),
-		Name:     "User Test",
-		CPF:      "11111111111",
-		Email:    "user@backend.com",
-		Password: "senha123",
-		Wallet:   200,
+	commonUser := schemas.User{
+		UUID:       uuid.NewString(),
+		Name:       "User Test",
+		Document:   "11111111111",
+		Email:      "user@backend.com",
+		Password:   "senha123",
+		Wallet:     100.00,
+		CommonUser: true,
 	}
-	shopkeeper := schemas.Shopkeeper{
-		UUID:     uuid.NewString(),
-		Name:     "Shopkeeper",
-		CNPJ:     "11111111111111",
-		Email:    "shopkeeper@backend.com",
-		Password: "senha123",
-		Wallet:   0,
+	shopkeeper := schemas.User{
+		UUID:       uuid.NewString(),
+		Name:       "Shopkeeper",
+		Document:   "11111111111111",
+		Email:      "shopkeeper@backend.com",
+		Password:   "senha123",
+		Wallet:     0.00,
+		CommonUser: false,
 	}
 
-	_ = insertDefault(r, user, shopkeeper)
-	//defer f()
+	f := insertDefault(r, commonUser, shopkeeper)
+	defer f()
 
 	type args struct {
-		w        *httptest.ResponseRecorder
-		r        *http.Request
+		w *httptest.ResponseRecorder
+		r *http.Request
 	}
 
 	payload := schemas.Transfer{
-		Value: 100,
-		Payer: user.UUID,
+		Value: 200.00,
+		Payer: commonUser.UUID,
 		Payee: shopkeeper.UUID,
 	}
 	b, err := json.Marshal(payload)
@@ -71,25 +84,25 @@ func TestMakeTransfer(t *testing.T) {
 		description string
 		args        args
 		wantErr     bool
-		expected	error
+		expected    int
 	}{
 		{
 			description: "user send money to other user with success",
 			args: args{
-				w:        httptest.NewRecorder(),
-				r:        httptest.NewRequest(http.MethodGet, "/transaction", bytes.NewReader(b)),
+				w: httptest.NewRecorder(),
+				r: httptest.NewRequest(http.MethodGet, "/transaction", bytes.NewReader(b)),
 			},
-			wantErr: false,
-			expected: nil,
+			wantErr:  false,
+			expected: http.StatusCreated,
 		},
 		{
 			description: "user send money with error",
 			args: args{
-				w:        httptest.NewRecorder(),
-				r:        httptest.NewRequest(http.MethodGet, "/transaction", bytes.NewReader(b)),
+				w: httptest.NewRecorder(),
+				r: httptest.NewRequest(http.MethodGet, "/transaction", bytes.NewReader(b)),
 			},
-			wantErr: true,
-			expected: error2.ErrInvalidBalance,
+			wantErr:  true,
+			expected: http.StatusBadRequest,
 		},
 	}
 
@@ -98,43 +111,77 @@ func TestMakeTransfer(t *testing.T) {
 			TransactionHandler(c.args.w, c.args.r)
 		})
 		resp := c.args.w.Result()
-		b, err := ioutil.ReadAll(resp.Body)
-		if resp.StatusCode != http.StatusCreated {
-			t.Errorf(string(b))
-			return
-		}
-
+		_, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
 			t.Error(err)
 			return
 		}
+
 		resp.Body.Close()
 
-		tCreated := schemas.Transfer{}
-		err = json.Unmarshal(b, &tCreated)
-		if !c.wantErr && err != nil {
-			t.Errorf("no error expected but has: %v", err)
-			return
-		}
-
-		if c.wantErr && err != c.expected {
+		if c.wantErr && resp.StatusCode != c.expected {
 			t.Errorf("expected: %v\n has: %v\n", c.expected, err)
 			return
 		}
 	}
 }
 
-func TestTransactionHandlerErr(t *testing.T) {
-	//testCases :=
+func Test_validTransaction(t *testing.T) {
+	setEnvs()
+	r, err := database.Open(context.Background())
+	if err != nil {
+		t.Errorf("expected no errors but was %v", err)
+		return
+	}
+
+	commonUser := schemas.User{
+		UUID:       uuid.NewString(),
+		Name:       "User Test",
+		Document:   "11111111111",
+		Email:      "user@backend.com",
+		Password:   "senha123",
+		Wallet:     100.00,
+		CommonUser: true,
+	}
+	shopkeeper := schemas.User{
+		UUID:       uuid.NewString(),
+		Name:       "Shopkeeper",
+		Document:   "11111111111111",
+		Email:      "shopkeeper@backend.com",
+		Password:   "senha123",
+		Wallet:     0.00,
+		CommonUser: false,
+	}
+
+	f := insertDefault(r, commonUser, shopkeeper)
+	defer f()
+
+	testCases := []struct {
+		description string
+		value       float64
+		expected    bool
+	}{
+		{
+			description: "invalid transaction",
+			value:       200,
+			expected:    false,
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.description, func(t *testing.T) {
+			valid, err := validTransaction(context.Background(), commonUser.UUID, 200, r)
+			if err != error2.ErrInvalidTransaction {
+				t.Errorf("expected %v but has %v", error2.ErrInvalidTransaction, err)
+				return
+			}
+			if valid != test.expected {
+				t.Errorf("expected %v but has %v", valid, test.expected)
+				return
+			}
+
+		})
+	}
+
 }
 
-func Test_notify(t *testing.T) {
-	 _ = notify(schemas.Transfer{
-		UUID:   uuid.NewString(),
-		Value:  100,
-		Payer:  uuid.NewString(),
-		Payee:  uuid.NewString(),
-		Status: "done",
-	})
-
-}
