fd9fb7a60662573bfe8815f40c8d987fbb927d66
Hemillainy Santos <hemillainy.santos@ccc.ufcg.edu.br>
2021-07-05 10:37:40 -0300

    mount the URL of the bank

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -5,5 +5,5 @@ echo $BACKEND_PASSWORD
 echo $BACKEND_DB_HOST
 echo $BACKEND_DB_PORT
 echo $BACKEND_DB_NAME
-/app/migrate.linux-amd64 -path /app/migrations/ -database "$DB_SOURCE" up
+/app/migrate.linux-amd64 -path /app/migrations/ -database postgres://$BACKEND_USER:$BACKEND_PASSWORD@$BACKEND_DB_HOST:$BACKEND_DB_PORT/$BACKEND_DB_NAME\?sslmode=disable up
 ./backend
