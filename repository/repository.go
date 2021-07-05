72f572733ec7f1411f4e92cf57dc5d055fb7d50c
Hemillainy Santos <hemillainy.santos@ccc.ufcg.edu.br>
2021-07-05 10:07:11 -0300

    remove some functions

--- repository/repository.go
+++ repository/repository.go
@@ -9,9 +9,7 @@ type IRepository interface {
 	CreateTransfer(ctx context.Context, transfer schemas.Transfer) (schemas.Transfer, error)
 	SelectUser(ctx context.Context, payerUUID string) (u schemas.User, err error)
 	CreateUser(ctx context.Context, u schemas.User) (err error)
-	CreateShopkeeper(ctx context.Context, s schemas.Shopkeeper) (err error)
 	DeleteUser(ctx context.Context, UUID string) (err error)
-	DeleteShopkeeper(ctx context.Context, UUID string) (err error)
-	UpdateUserWallet(ctx context.Context, value float64, UUID string) (error)
-	UpdateShopkeeperWallet(ctx context.Context, value float64, UUID string) (error)
+	UpdateUserWallet(ctx context.Context, value float64, UUID string, commonUser bool) (schemas.User, error)
+	Close() (err error)
 }
